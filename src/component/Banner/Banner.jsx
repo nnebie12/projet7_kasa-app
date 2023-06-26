5aab2f0ba02238b983f9890ca86e0ad8e21c4590
KASSI Diane Rolande <kassinnebie@macbook-pro-de-kassi.home>
2023-06-26 22:54:30 +0200

    création de dossiers header, footer et banner

---
+++ src/component/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import '../Banner.scss'
+
+export default function Banner() {
+	return (
+		<div className="banner-container">
+			<div className="banner-text-container">
+				<div className="banner-text">Chez vous, partout et ailleurs</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,14 @@
+import logo from "../../assets/log.svg";
+import Nav from "../Nav/Nav";
+
+export default function Header() {
+	return (
+		<header className="headerWrap">
+			<figure className="headerWrap__fig">
+				<img className="logo" src={logo} alt="logo de l'agence kasa" />
+			</figure>
+			<Nav className="nav-header" />
+		</header>
+	);
+}
+
