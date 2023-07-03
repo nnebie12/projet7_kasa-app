3faac8a43127c61889a55841845f9e6b98024fe8
KASSI Diane Rolande <kassinnebie@macbook-pro-de-kassi.home>
2023-07-03 19:03:56 +0200

    fichier banner fait
    navigation mise en place
    
    fichier banner.css terminé
    
    header mis en place
    
    mise en place de la card
    
    mise en place de AproposPages
    
    mis en place de la page d'erreur
    
    mise en place du fichier logement
    
    image bannerApropos changé
    
    page logement mis en place
    
    ajout de commentaire dans le code
    
    mise à jour dans le fichier logementsPages
    
    correction du code dans le fichier logementsPages
    
    mise à jour de la responsive
    
    correction

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,27 +1,27 @@
 import Banner from "../../components/Banner";
-import Card from "../../components/Card";
-import styles from "../../styles/Home.css"
-import BannerHeader from "../../assets/BannerHeader.png"
-import logements from '../../data/logements.json'
+import "../../styles/Card.css";
+import logementsData from '../../data/logements.json'
+import BannerHeader from '../../assets/BannerHeader.png'
+import Card from '../../components/Card'
 
 
 
 
 function Home() {
 	return (
-	   <div>
-		  <Banner image={BannerHeader} text="Chez vous, partout et ailleurs" />
-		  <div className={styles.cardContainer}>
-			 {logements.map((logements) => (
-				<Card
-				   key={logements.id}
-				   id={logements.id}
-				   cover={logements.cover}
-				   title={logements.title}
-				/>
-			 ))}
-		  </div>
-	   </div>
+		<div>
+         <Banner image={BannerHeader} text="Chez vous, partout et ailleurs" />
+         <div className="cardContainer">
+            {logementsData.map((logements) => (
+               <Card
+                  key={logements.id}
+                  id={logements.id}
+                  cover={logements.cover}
+                  title={logements.title}
+               />
+            ))}
+         </div>
+      </div>
 	);
  }
  
